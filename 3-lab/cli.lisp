0f9d81d8b559c61d010ae57d1fd2e6bac9f46204
pavlo myroniuk <pspos.developqkation@gmail.com>
2020-02-29 22:24:15 +0200

    now can parse .json; finish 3-lab

--- 3-lab/cli.lisp
+++ 3-lab/cli.lisp
@@ -1,12 +1,18 @@
 
 (require 'asdf)
-(load "/home/qkation/Documents/FunctionalProgramming/3-lab/cl-simple-table-master/cl-simple-table.asd")
+; load package for .csv and .tsv parsing
+(load "/home/qkation/Documents/LispFunctionalProgramming/3-lab/cl-simple-table-master/cl-simple-table.asd")
 (asdf:load-system 'cl-simple-table)
+; load package for .json parsing
+(load "/home/qkation/Documents/LispFunctionalProgramming/3-lab/cl-json_0.5.0/cl-json.asd")
+(asdf:load-system 'cl-json)
 
+; load parse files and save data to variables
 (defvar map_zal (simple-table:read-csv #P"map_zal-skl9.csv" t))
 (defvar mp_assistants (simple-table:read-csv #P"mp-assistants.csv" t))
 (defvar mp_posts (simple-table:read-csv #P"mp-posts_full.csv"))
 (defvar plenary_register_mps (simple-table:read-tsv #P"plenary_register_mps-skl9.tsv"))
+(defvar mps_declarations_rada(json:decode-json (open "./mps-declarations_rada.json")))
 
 (defun printTable(simple_table row)(cond
 								 ((= row 0) (pprint (simple-table:get-row 0 simple_table)))
@@ -23,6 +29,8 @@
 							 (printTable mp_posts (- (simple-table:num-rows mp_posts) 1)))
 							((string= tableName "plenary_register_mps-skl9")
 							 (printTable plenary_register_mps (- (simple-table:num-rows plenary_register_mps) 1)))
+							((string= tableName "mps-declarations_rada")
+							 (printTable mps_declarations_rada (- (simple-table:num-rows mps_declarations_rada) 1)))
 							(t (princ (concatenate 'string "table not found: " tableName)))
 						  )
 )
--- 3-lab/test.lisp
+++
@@ -1,9 +0,0 @@
-
-(defun condd(i)(cond ((= i 5) 
-							   (write 5)
-							   (write "weferf")
-							   )
-					 (t (condd 5)(write "pasha")(write "pacha"))
-					 ))
-
-(condd 6)
