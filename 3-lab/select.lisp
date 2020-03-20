eb5cc45c3fe1643d2decd66bf1324f676a7f91db
pavlo myroniuk <pspos.developqkation@gmail.com>
2020-03-20 11:59:35 +0200

    implemented aggregate functions: count, max and avg

--- 3-lab/select.lisp
+++ 3-lab/select.lisp
@@ -5,18 +5,99 @@
 
 (defun countRows (column table)
   "count aggregate funciton"
-  (concatenate 'string "count -> " (write-to-string (car column)))
+  (cond
+	((= (length column) 1) (let ((index (nth 0 column)))
+	 						 (reduce (lambda (cnt elem)
+									 (cond
+									   ((not (aref elem index)) cnt)
+									   (t (+ cnt 1))
+									   )
+									 )
+									 table
+									 :initial-value 0)
+							 ))
+	((= (length column) (length (aref table 0))) (length table))
+	(t "Error: Wrong arguments for count() function!")
+	)
+  )
+
+(defun getComparator (value)
+  (cond
+        ((numberp value) #'<)
+        ((stringp value) #'string<)
+        (t (lambda (v1 v2)T))
+        )
+  )
+
+(defun findMax (index table)
+  "this finction finds max value in given column"
+  (reduce (lambda (maxValue row)
+			(let ((elem (aref row index)))
+   			  (cond
+   			    ; if maxValue is nil and elem is not nil
+   			    ((and (not maxValue) elem) elem)
+   			    ; if elem is nil then we return maxValue
+   			    ((not elem) maxValue)
+   			    ; standart case: we compare two numbers. if maxValue < elem, then return elem
+   			    ((funcall (getComparator elem) maxValue elem) elem)
+   			    ; else return maxValue
+   			    (t maxValue)
+   			    )
+			  )
+			)
+		  table
+		  :initial-value nil)
   )
 
 (defun maxRows (column table)
   "max aggregate funciton"
-  (concatenate 'string "max -> " (write-to-string (car column)))
+  (cond
+	((= (length column) 1) (findMax (nth 0 column) table))
+	(t "Error: Wrong arguments for max() function!")
+	)
+  )
+
+(defun findAverage (index table)
+  "this funciton determine average value in given column"
+  (let ((sum 0) (amount 0))
+	(setq sum (reduce (lambda (sum row)
+					   (let ((elem (aref row index)))
+						 (cond
+						   ((not elem) sum)
+						   (t (setq amount (+ amount 1))
+							  (+ sum elem))
+						   )
+						 )
+			           )
+					 table
+					 :initial-value 0))
+	(pprint sum)
+	(pprint amount)
+	(cond
+	  ((= amount 0) nil)
+	  (t (/ (float sum) amount))
+	  )
+	)
   )
 
 (defun avgRows (column table)
   "average aggregate funciton"
-  (concatenate 'string "avg -> " (write-to-string (car column)))
+  (cond
+	((= (length column) 1) (findAverage (nth 0 column) table))
+	(t "Error: Wrong arguments for max() function!")
+	)
   )
+#||
+(pprint (avgRows '(0) #(
+						  #(1 9 3)
+						  #(4 nil 6)
+						  #(7 8 9)
+						  #(10 nil 12)
+						  )
+				   )
+		)+
+(exit)
+;||#
 
 ; hashmap that contain aggregate functions where key is afunctionName and value is a funciton
 (defvar aggregateFunctions (make-hash-table :test 'equal))
@@ -81,6 +162,7 @@
 	(t (selectAggregateFunctions columns table tableIndexes (make-array 0 :fill-pointer 0)))
 	)
   )
+
 #||
 (defvar indexes (make-hash-table :test 'equal))
 (setf (gethash "col1" indexes) '(0))
