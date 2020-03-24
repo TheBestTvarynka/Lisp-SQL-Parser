020f896d20d1679db564031901f0a9a7c56de895
pavlo myroniuk <pspos.developqkation@gmail.com>
2020-03-24 20:47:19 +0200

    move function for printing table in separate file: print.lisp

--- 3-lab/cli.lisp
+++ 3-lab/cli.lisp
@@ -1,5 +1,6 @@
 
 ; load all functionality code
+(load "print.lisp")
 (load "importer.lisp")
 (load "distinct.lisp")
 (load "where.lisp")
@@ -14,32 +15,6 @@
 (setf (gethash "mps-declarations_rada" tables) (readTableFromFile "datasource/mps-declarations_rada.json"))
 (setf (gethash "test" tables) (readTableFromFile "datasource/test.csv"))
 ;(setf (gethash "plenary_register_mps-skl9" tables) mps_declarations_rada)
-
-(defun len (value)
-  "returns length of value"
-  (cond
-	((not value) 3)
-	((stringp value) (length value))
-	((numberp value) (length (write-to-string value)))
-	(t 1)
-	)
-  )
-
-(defun countWidths (table)
-  "returns array that contain width of each column of the table"
-  (let ((columnsAmount (length (aref table 0))))
-    (reduce (lambda (widths row)
-  			(map 'vector
-  				 (lambda (width elem)
-  				   (max width (len elem))
-  				   )
-  				 widths
-  				 row)
-  			)
-  		  table
-  		  :initial-value (make-array columnsAmount :fill-pointer columnsAmount))
-	)
-  )
 
 (defun getTableName (queryStr)
   "cut table name from query"
@@ -133,48 +108,6 @@
 	  (lambda (value)(string-trim " " value))
 	  (simple-table:split-string #\, queryStr))
 	)
-  )
-
-(defun getFormatString (width elem)
-  "construct control-string for format function"
-  (cond
-	((stringp elem) (concatenate 'string "~" (write-to-string width) "A"))
-	((numberp elem) (concatenate 'string "~" (write-to-string width) "D"))
-	(t (concatenate 'string "~" (write-to-string width) "A"))
-	)
-  )
-
-(defun printRow (widths row)
-  "this function just print row"
-  (let ((lastWidth (aref widths (- (length widths) 1))) (lastElem (aref row (- (length row) 1))))
-    (map 'list
-  	     (lambda (width elem)
-  		   (princ (format nil (getFormatString width elem) elem))
-		   (princ " | ")
-  		   )
-  		  widths
-  		  (delete-if (constantly t) row :count 1 :from-end t))
-	(princ (format nil (getFormatString lastWidth lastElem) lastElem))
-	)
-  )
-
-(defun printTable (resultTable)
-  "just print table function in pretty way"
-	(let ((widths (countWidths (formatTable resultTable))))
-	  (printRow widths (table-columnNames resultTable))
-	  (terpri)
-	  (princ (format nil "~v@{~A~:*~}" (+ (reduce (lambda (sum width)
-										            (+ sum width)
-										            )
-										          widths) (* (- (length widths) 1) 3)) #\-))
-	  (terpri)
-	  (map 'list
-		   (lambda (row)
-		     (printRow widths row)
-		     (terpri)
-			 )
-		   (table-data resultTable))
-		)
   )
 
 (defun query (queryStr)
