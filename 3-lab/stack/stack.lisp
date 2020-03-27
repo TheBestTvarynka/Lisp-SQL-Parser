ed840b6cffd887dfe22c867303eaf3e2a5880aab
pavlo myroniuk <pspos.developqkation@gmail.com>
2020-03-27 21:37:42 +0200

    add stack to the project (written by me)

---
+++ 3-lab/stack/stack.lisp
@@ -0,0 +1,47 @@
+
+
+(defstruct stack
+  (list '() :type list)
+  )
+
+(defun stack-push (value stack)
+  (setf (stack-list stack) (append (list value) (stack-list stack)))
+  stack
+  )
+
+(defun stack-pop (stack)
+  (setf (stack-list stack) (cdr (stack-list stack)))
+  stack
+  )
+
+(defun stack-top (stack)
+  (car (stack-list stack))
+  )
+
+(defun stack-is-empty (stack)
+  (cond
+	((not (stack-list stack)) T)
+	(t nil)
+	)
+  )
+
+(defun stack-size (stack)
+  (length (stack-list stack))
+  )
+
+#||
+(defvar simple-stack (make-stack))
+(setf simple-stack (stack-push "pasha" simple-stack))
+(pprint simple-stack)
+(setf simple-stack (stack-push "pacha" simple-stack))
+(pprint simple-stack)
+(pprint (stack-top simple-stack))
+(pprint (stack-size simple-stack))
+(pprint (stack-is-empty simple-stack))
+(setf simple-stack (stack-pop simple-stack))
+(pprint simple-stack)
+(setf simple-stack (stack-pop simple-stack))
+(pprint simple-stack)
+(pprint (stack-is-empty simple-stack))
+;||#
+
